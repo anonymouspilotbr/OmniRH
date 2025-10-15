5819875661fdb93cd6f1ff926e9c28ff9f351d53
anonymouspilotbr <ybrasileiro6@gmail.com>
2025-10-15 20:42:08 -0300

    [0.12.1] Configurado o banco de dados remoto.

--- src/model/db.js
+++ src/model/db.js
@@ -2,11 +2,7 @@ const pg = require('pg');
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'OmniRH',
-    password: 'uemeeli',
-    port: 5432
+    connectionString: 'postgresql://neondb_owner:npg_9p5BFyRYXMfW@ep-tiny-hall-acx7j26v-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 });
 
 module.exports = pool;
