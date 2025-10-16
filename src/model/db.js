632288cdf0d02a0cde7e59005da89bc9bc122aa7
anonymouspilotbr <ybrasileiro6@gmail.com>
2025-10-16 17:41:01 -0300

    [0.12.1B] Minor fix in db.js

--- src/model/db.js
+++ src/model/db.js
@@ -2,7 +2,7 @@ const pg = require('pg');
 const { Pool } = pg;
 
 const pool = new Pool({
-    connectionString: 'postgresql://neondb_owner:npg_9p5BFyRYXMfW@ep-tiny-hall-acx7j26v-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+    connectionString: 'postgresql://neondb_owner:npg_9p5BFyRYXMfW@ep-tiny-hall-acx7j26v-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require'
 });
 
 module.exports = pool;
