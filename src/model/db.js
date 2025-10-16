41effef55f159d51c7002ea7e60ac9ba5940cf75
anonymouspilotbr <ybrasileiro6@gmail.com>
2025-10-16 18:28:21 -0300

    [0.12.2] ATUALIZAÇÃO DE SEGURANÇA

--- src/model/db.js
+++ src/model/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const pg = require('pg');
 const { Pool } = pg;
 
 const pool = new Pool({
-    connectionString: 'postgresql://neondb_owner:npg_9p5BFyRYXMfW@ep-tiny-hall-acx7j26v-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require'
+    connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
