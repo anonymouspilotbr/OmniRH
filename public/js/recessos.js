57828d4fd07eaef686e85abda95a6912c6edd401
anonymouspilotbr <ybrasileiro6@gmail.com>
2025-11-21 17:52:46 -0300

    [0.19.1] Desenvolvimento Recessos I

---
+++ public/js/recessos.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        fetch("https://omnirh.onrender.com/me", {
+            headers: { "Authorization": `Bearer ${token}` }
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (!data || !data.nome) return console.warn("Usuário não encontrado.");
+
+            const id_funcionario = data.id;
+            
+            
+        
+        })
+    }
+})
--- src/controllers/generalController.js
+++ src/controllers/generalController.js
@@ -29,7 +29,7 @@ function licencasView(req,res){
 }
 
 function recessosView(req,res){
-    res.render('Omni-RH.Recessos.html');
+    res.render('recessosv2.html');
 }
 
 function recessosRHView(req,res){
@@ -55,35 +55,6 @@ function esqueciSenhaView(req,res){
 function resetarSenhaView(req, res){
     res.render('resetar_senha.html');
 }
-
-/*exports.register = (req, res) => {
-    const { email, senha } = req.body;
-
-    if (!email || !senha)
-        return res.status(400).json({ msg: 'Preencha todos os campos' });
-
-    const userExists = users.find(u => u.email === email);
-    if (userExists)
-        return res.status(400).json({ msg: 'Usuário já cadastrado' });
-
-    const hashedPassword = bcrypt.hashSync(senha, 8);
-    users.push({ email, senha: hashedPassword });
-
-    res.json({ msg: 'Usuário registrado com sucesso!' });
-};
-
-exports.login = (req, res) => {
-    const { email, senha } = req.body;
-
-    const user = users.find(u => u.email === email);
-    if (!user) return res.status(404).json({ msg: 'Usuário não encontrado' });
-
-    const senhaValida = bcrypt.compareSync(senha, user.senha);
-    if (!senhaValida) return res.status(401).json({ msg: 'Senha incorreta' });
-
-    const token = jwt.sign({ email: user.email }, SECRET, { expiresIn: '1h' });
-    res.json({ msg: 'Login realizado com sucesso', token });
-};*/
 
 module.exports = {
     loginView,
