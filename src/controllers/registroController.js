7e91c4f1bb64f761f07d63d03a081788f4932985
anonymouspilotbr <ybrasileiro6@gmail.com>
2025-11-25 18:10:42 -0300

    [0.20.1] Desenvolvimento Folha de Ponto I

--- src/controllers/registroController.js
+++ src/controllers/registroController.js
@@ -1,41 +1,7 @@
 const registroService = require('../service/registroService');
 
-/*async function registrarEntrada(req, res) {
-  const { usuario_id, entrada } = req.body;
-  if (!usuario_id || !entrada) {
-    return res.status(400).json({ error: 'Dados incompletos' });
-  }
-
-  try {
-    const registro = await registroService.registrarEntrada(usuario_id, entrada);
-    res.status(201).json({ message: 'Entrada registrada', id: registro.id });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Erro ao registrar entrada' });
-  }
-}
-
-async function registrarSaida(req, res) {
-  const { registro_id } = req.params;
-  const { saida } = req.body;
-  if (!saida) {
-    return res.status(400).json({ error: 'Horário de saída obrigatório' });
-  }
-
-
-
-  try {
-    const { horas, extras } = await registroService.registrarSaida(registro_id, saida);
-    res.json({ message: 'Saída registrada', horas, extras });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Erro ao registrar saída' });
-  }
-}*/
-
 async function registrarPonto(req, res) {
     try {
-        console.log("req.user =", req.user);
         const idFuncionario = req.user.id; 
         const resultado = await registroService.registrarPonto(idFuncionario);
         res.json(resultado);
