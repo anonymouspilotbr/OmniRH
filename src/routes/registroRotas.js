80d2eae8b6fab2b33a964d338bf6c10710b4a56c
anonymouspilotbr <ybrasileiro6@gmail.com>
2025-11-25 17:41:34 -0300

    [0.20.0_7] Debug Auth

--- src/routes/registroRotas.js
+++ src/routes/registroRotas.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const registroController = require('../controllers/registroController');
+const authController = require('../controllers/authController');
 
 //router.post('/entrada', registroController.registrarEntrada);
 //router.put('/saida/:registro_id', registroController.registrarSaida);
-router.post('/', registroController.registrarPonto);
+router.post('/', authController.autenticarToken, registroController.registrarPonto);
 router.get('/:usuario_id', registroController.listar);
 router.get('/:id_funcionario/semana/:data_inicio', registroController.listarSemana);
 
